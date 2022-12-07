3878f0c3cc4ca7b736b9db8b038da7bb0c5d0954
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-12-08 00:40:58 +0200

    finish

--- contacts.js
+++ contacts.js
@@ -1,25 +1,73 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { program } = require('commander');
-require('colors');
 
-const contactsPath = '';
+const contactsPath = path.resolve('./db/contacts.json');
 
 function listContacts() {
-  console.log('List');
-  // ...твой код
+  fs.readFile(contactsPath, 'utf-8')
+    .then(data => {
+      console.table(data);
+      if (!data) {
+        console.warn('there no contacts!!');
+        return;
+      }
+      return JSON.parse(data);
+    })
+    .then(result => console.table(result))
+    .catch(error => console.error(error));
 }
 
 function getContactById(contactId) {
-  // ...твой код
-  console.log('getContactById', contactId);
+  fs.readFile(contactsPath, 'utf-8')
+    .then(data => JSON.parse(data))
+    .then(result => result.find(contact => Number(contact.id) === contactId))
+    .then(result => {
+      console.table(result);
+    })
+    .catch(error => console.error(error));
 }
 
 function removeContact(contactId) {
-  // ...твой код
+  fs.readFile(contactsPath, 'utf-8')
+    .then(data => JSON.parse(data))
+    .then(result =>
+      result.filter(contact => {
+        if (contact.id !== toString(contactId)) {
+          return contact;
+        }
+      }),
+    )
+    .then(result => {
+      fs.writeFile(contactsPath, JSON.stringify(result), 'utf-8');
+      console.table(result);
+    })
+    .catch(error => console.error(error));
+}
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, 'utf-8')
+    .then(data => {
+      if (!data) {
+        console.warn('there no contacts');
+        return;
+      }
+      return JSON.parse(data);
+    })
+    .then(result => {
+      const id = JSON.stringify(getMaxId(result) + 1);
+      return [...result, { id, name, email, phone }];
+    })
+    .then(result => {
+      console.table(result);
+      fs.writeFile(contactsPath, JSON.stringify(result), 'utf-8');
+    })
+    .catch(error => console.error(error));
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+function getMaxId(contactsList) {
+  return contactsList.reduce(
+    (maxId, contact) => (Number(contact.id) > maxId ? (newId = Number(contact.id)) : maxId),
+    0,
+  );
 }
+
 module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const argv = require('yargs').argv;
+// const argv = require('yargs').argv;
 const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
-// TODO: рефакторить
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
+      listContacts();
       break;
 
     case 'get':
       // ... id
+      getContactById(id);
       break;
 
     case 'add':
       // ... name email phone
+      addContact(name, email, phone);
       break;
 
     case 'remove':
       // ... id
+      removeContact(id);
       break;
 
     default:
@@ -25,5 +40,3 @@ function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-console.log('wedwf');
-listContacts();
